9b5cda999f5036b8b659c85d5c8e9749c14d5f2e
singhguri <singhguri5598@gmail.com>
2024-01-19 16:30:51 -0500

    updated init with db connection string from .env

--- app/__init__.py
+++ app/__init__.py
@@ -10,41 +10,38 @@ from sqlalchemy import create_engine
 
 from datetime import timedelta
 
- 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.urandom(24)
+app.config["SECRET_KEY"] = os.urandom(24)
 
 CORS(app, supports_credentials=True)
 
-app.config['SESSION_TYPE'] = 'filesystem'  # Can be any session type you prefer
+app.config["SESSION_TYPE"] = "filesystem"  # Can be any session type you prefer
 
-app.config['SESSION_COOKIE_SECURE'] = True  # Ensure secure session cookie for production
+app.config[
+    "SESSION_COOKIE_SECURE"
+] = True  # Ensure secure session cookie for production
 
-#app.config['SESSION_COOKIE_HTTPONLY'] = True  # Restrict session cookie access to HTTP-only
+# app.config['SESSION_COOKIE_HTTPONLY'] = True  # Restrict session cookie access to HTTP-only
 
-app.config['SESSION_COOKIE_SAMESITE'] = 'None'  # Apply strict same-site policy
+app.config["SESSION_COOKIE_SAMESITE"] = "None"  # Apply strict same-site policy
 
-
-
- 
-
- 
 
 # DB connection
 
-conn = conn = psycopg2.connect('postgresql://ealehddbpmkhay:2954cd87066a4ce0990c0c5b6372fcf4dc5a52f82bcee95d601121f3e2e7dd35@ec2-44-208-206-97.compute-1.amazonaws.com:5432/d9d8b0htabq5eb')
+conn = conn = psycopg2.connect(os.environ.get("DB_CONNECTION_STRING"))
 
-engine = create_engine('postgresql://ealehddbpmkhay:2954cd87066a4ce0990c0c5b6372fcf4dc5a52f82bcee95d601121f3e2e7dd35@ec2-44-208-206-97.compute-1.amazonaws.com:5432/d9d8b0htabq5eb')
+engine = create_engine(os.environ.get("DB_CONNECTION_STRING"))
 
-@app.route('/')
+
+@app.route("/")
 def testmain():
-    return 'testing'
+    return "testing"
+
 
 # postgres://keshavsharma:8ouqic74GL1gRV6HFYzkfiyIgiVNdN8A@dpg-ckq1iehrfc9c73egiev0-a.oregon-postgres.render.com/cuma
 
- 
 
 # include for routing should always at the end of file
 
