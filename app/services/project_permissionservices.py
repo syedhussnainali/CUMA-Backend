894a0109dadfa4a2dc2ca37af418407121901551
singhguri <singhguri5598@gmail.com>
2024-01-31 15:32:55 -0500

    updated requirements.txt

--- app/services/project_permissionservices.py
+++ app/services/project_permissionservices.py
@@ -4,11 +4,11 @@ from flask import jsonify, session
 from ..model.project_permissions import ProjectPermissions
 from ..model.project import Project
 
-#read_write_flag 
+
+# read_write_flag
 #   true if read and write
 #   flase if read
 def checkPermissions(project_id, user_id, session, read_write_flag):
-    
     project = aliased(Project)
     projectPermissions = aliased(ProjectPermissions)
 
@@ -27,14 +27,20 @@ def checkPermissions(project_id, user_id, session, read_write_flag):
         leftConditions = default_read
     conditions = or_(leftConditions, rightConditions)
 
-    query = session.query(projectPermissions.user_id, project.id) \
-        .join(project, project.id == projectPermissions.project_id) \
-        .filter(condition1) \
+    query = (
+        session.query(projectPermissions.user_id, project.id)
+        .join(project, project.id == projectPermissions.project_id)
+        .filter(condition1)
         .filter(conditions)
-    print(query.statement.compile(dialect=session.bind.dialect, compile_kwargs={"literal_binds": True}))
+    )
+    print(
+        query.statement.compile(
+            dialect=session.bind.dialect, compile_kwargs={"literal_binds": True}
+        )
+    )
     results = query.count()
-    #print(results)
-    if results is 0:
+    # print(results)
+    if results == 0:
         return False
     else:
         return True
